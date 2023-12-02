94aa29611611f8370f65688b2f365dbb4a15e21d
Kevin Day <kevinday.home@gmail.com>
2023-12-02 00:59:44 -0600

    Day 3 prep

--- 2023/Day03/Program.cs
+++ 2023/Day03/Program.cs
@@ -3,4 +3,20 @@ using static Library.Geometry;
 using static Library.Optimize;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+void part1(string file_name)
+{
+    string[] lines = readFileLines(file_name);
+    Console.WriteLine($"Part 1 - {file_name}:");
+}
+
+void part2(string file_name)
+{
+    string[] lines = readFileLines(file_name);
+    Console.WriteLine($"Part 2 - {file_name}:");
+}
+
+part1("sample.txt");
+part1("input.txt");
+
+part2("sample.txt");
+part2("input.txt");
