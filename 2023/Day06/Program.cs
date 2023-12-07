a6a76ffac0e1db89d56e2ab1fa5748ac0c801998
Kevin Day <kevinday.home@gmail.com>
2023-12-06 18:44:59 -0600

    Day 6

--- 2023/Day06/Program.cs
+++ 2023/Day06/Program.cs
@@ -1,6 +1,34 @@
 using Library;
-using static Library.Geometry;
-using static Library.Optimize;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+void part1(string file_name)
+{
+    string[] lines = readFileLines(file_name);
+    var times = lines[0].ExtractLongs().ToList();
+    var distances = lines[1].ExtractLongs().ToList();
+
+    List<long> ways = new();
+    for(int i = 0; i < times.Count(); ++i)
+    {
+        long min = (long)(Math.Floor((-times[i] + Math.Sqrt((times[i] * times[i]) - (4 * distances[i]))) / -2) + 1);
+        long max = (long)(Math.Ceiling((-times[i] - Math.Sqrt((times[i] * times[i]) - (4 * distances[i]))) / -2) - 1);
+        ways.Add(max - min + 1);
+    }
+    Console.WriteLine($"Part1 - {file_name}: {ways.Product()}");
+}
+
+void part2(string file_name)
+{
+    string[] lines = readFileLines(file_name);
+    var time = long.Parse(string.Join("", lines[0].Split(' ').Skip(1)));
+    var distance = long.Parse(string.Join("", lines[1].Split(' ').Skip(1)));
+
+    long min = (long)(Math.Floor((-time + Math.Sqrt((time * time) - (4 * distance))) / -2) + 1);
+    long max = (long)(Math.Ceiling((-time - Math.Sqrt((time * time) - (4 * distance))) / -2) - 1);
+    Console.WriteLine($"Part1 - {file_name}: {max - min + 1}");
+}
+
+part1("sample.txt");
+part1("input.txt");
+part2("sample.txt");
+part2("input.txt");
