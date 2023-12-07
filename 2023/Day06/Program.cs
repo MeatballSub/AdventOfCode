3f334a7761cae00835363d4bbc035ce4d4eebc70
Kevin Day <kevinday.home@gmail.com>
2023-12-06 19:17:49 -0600

    Refactor Day 6 a little

--- 2023/Day06/Program.cs
+++ 2023/Day06/Program.cs
@@ -1,31 +1,23 @@
 using Library;
 using static Library.Parsing;
 
+long getWays(long time, long distance)
+{
+    long min = (long)(Math.Floor((-time + Math.Sqrt((time * time) - (4 * distance))) / -2) + 1);
+    long max = (long)(Math.Ceiling((-time - Math.Sqrt((time * time) - (4 * distance))) / -2) - 1);
+    return max - min + 1;
+}
+
 void part1(string file_name)
 {
-    string[] lines = readFileLines(file_name);
-    var times = lines[0].ExtractLongs().ToList();
-    var distances = lines[1].ExtractLongs().ToList();
-
-    List<long> ways = new();
-    for(int i = 0; i < times.Count(); ++i)
-    {
-        long min = (long)(Math.Floor((-times[i] + Math.Sqrt((times[i] * times[i]) - (4 * distances[i]))) / -2) + 1);
-        long max = (long)(Math.Ceiling((-times[i] - Math.Sqrt((times[i] * times[i]) - (4 * distances[i]))) / -2) - 1);
-        ways.Add(max - min + 1);
-    }
-    Console.WriteLine($"Part1 - {file_name}: {ways.Product()}");
+    long ans = readFileLines(file_name).Chunk(2).Select(_ => _[0].ExtractLongs().Zip(_[1].ExtractLongs(), (t, d) => getWays(t, d)).Product()).First();
+    Console.WriteLine($"Part1 - {file_name}: {ans}");
 }
 
 void part2(string file_name)
 {
-    string[] lines = readFileLines(file_name);
-    var time = long.Parse(string.Join("", lines[0].Split(' ').Skip(1)));
-    var distance = long.Parse(string.Join("", lines[1].Split(' ').Skip(1)));
-
-    long min = (long)(Math.Floor((-time + Math.Sqrt((time * time) - (4 * distance))) / -2) + 1);
-    long max = (long)(Math.Ceiling((-time - Math.Sqrt((time * time) - (4 * distance))) / -2) - 1);
-    Console.WriteLine($"Part1 - {file_name}: {max - min + 1}");
+    long ans = readFileLines(file_name).Select(_ => long.Parse(_.Split(':')[1].Replace(" ", ""))).Chunk(2).Select(_ => getWays(_[0], _[1])).First();
+    Console.WriteLine($"Part2 - {file_name}: {ans}");
 }
 
 part1("sample.txt");
