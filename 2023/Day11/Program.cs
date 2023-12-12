1096addaa151b18bf3cd845259b09c29d2e16bd5
Kevin Day <kevinday.home@gmail.com>
2023-12-11 20:06:27 -0600

    Day 11

--- 2023/Day11/Program.cs
+++ 2023/Day11/Program.cs
@@ -1,6 +1,56 @@
-using Library;
 using static Library.Geometry;
-using static Library.Optimize;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+long solve(string file_name, long expansion_factor)
+{
+    List<string> lines = readFileLines(file_name).ToList();
+    List<int> empty_rows = new();
+    List<int> empty_cols = Enumerable.Range(0, lines[0].Length).ToList();
+
+    for (int y = 0; y < lines.Count; y++)
+    {
+        if (!lines[y].Contains('#'))
+        {
+            empty_rows.Add(y);
+        }
+        for (int x = 0; x < lines[y].Length; x++)
+        {
+            if (lines[y][x] == '#')
+            {
+                empty_cols.Remove(x);
+            }
+        }
+    }
+
+    HashSet<Point> galaxies = new();
+    for (int y = 0; y < lines.Count; ++y)
+    {
+        long expand_y = empty_rows.Where(_ => _ < y).Count();
+        for (int x = 0; x < lines[y].Length; x++)
+        {
+            if (lines[y][x] == '#')
+            {
+                long expand_x = empty_cols.Where(_ => _ < x).Count();
+                galaxies.Add(new Point(x + (expansion_factor - 1) * expand_x, y + (expansion_factor - 1) * expand_y));
+            }
+        }
+    }
+
+    List<Point> galaxy_list = galaxies.ToList();
+
+    long sum = 0;
+    for (int i = 0; i < galaxy_list.Count; ++i)
+    {
+        for (int j = i + 1; j < galaxy_list.Count; j++)
+        {
+            sum += manhattanDistance(galaxy_list[i], galaxy_list[j]);
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine($"Part 1 - sample: {solve("sample.txt", 2)}");
+Console.WriteLine($"Part 1 - input: {solve("input.txt", 2)}");
+Console.WriteLine($"Part 2 - sample: {solve("sample.txt", 10)}");
+Console.WriteLine($"Part 2 - sample: {solve("sample.txt", 100)}");
+Console.WriteLine($"Part 2 - input: {solve("input.txt", 1000000)}");
