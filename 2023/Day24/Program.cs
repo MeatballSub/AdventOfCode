00b6b59f6ce90bdef1b34f38e2c942a5172d45c7
Kevin Day <kevinday.home@gmail.com>
2023-12-29 07:16:31 -0600

    Day 24

--- 2023/Day24/Program.cs
+++ 2023/Day24/Program.cs
@@ -7,16 +7,23 @@ using PointD = (double X, double Y);
 
 Random rand = new Random();
 
+int X = 0;
+int Y = 1;
+int Z = 2;
+int VX = 3;
+int VY = 4;
+int VZ = 5;
+
 bool IsParallel(List<long> a, List<long> b)
 {
     // note, not checking colinearity
-    return (a[4] * b[3] == b[4] * a[3]);
+    return (a[VY] * b[VX] == b[VY] * a[VX]);
 }
 
 double SolveT(List<long> a, List<long> b)
 {
-    double t = b[3] * (b[1] - a[1]) + b[4] * (a[0] - b[0]);
-    t /= (a[4] * b[3]) - (a[3] * b[4]);
+    double t = b[VX] * (b[Y] - a[Y]) + b[VY] * (a[X] - b[X]);
+    t /= (a[VY] * b[VX]) - (a[VX] * b[VY]);
     return t;
 }
 
@@ -27,7 +34,7 @@ double SolveT(List<long> a, List<long> b)
         return (false, new PointD(0, 0));
     }
 
-    double t0 = SolveT(a,b);
+    double t0 = SolveT(a, b);
     double t1 = SolveT(b, a);
 
     if (t0 < 0 || t1 < 0) // in the past
@@ -35,10 +42,10 @@ double SolveT(List<long> a, List<long> b)
         return (false, new PointD(0, 0));
     }
 
-    double x = a[0] + t0 * a[3];
-    double y = a[1] + t0 * a[4];
+    double x = a[X] + t0 * a[VX];
+    double y = a[Y] + t0 * a[VY];
 
-    return (true, new PointD(x,y));
+    return (true, new PointD(x, y));
 }
 
 bool InAreaSample(PointD p) => 7 <= p.X && p.X <= 27 && 7 <= p.Y && p.Y <= 27;
@@ -50,7 +57,7 @@ void part1(string file_name, InAreaFunc inArea)
     List<List<long>> input = readFileLines(file_name).ExtractSignedLongs().Select(e => e.ToList()).ToList();
     for(int i = 0; i < input.Count; i++)
     {
-        for(int j = i + 1; j < input.Count; j++)
+        for (int j = i + 1; j < input.Count; j++)
         {
             (bool status, PointD p) = Intersect(input[i], input[j]);
             if (status && inArea(p))
@@ -62,32 +69,34 @@ void part1(string file_name, InAreaFunc inArea)
     Console.WriteLine($"Part 1 - {file_name}: {count}");
 }
 
-double[,] BuildMatrix1(List<List<long>> input)
+List<List<double>> BuildMatrix1(List<List<long>> input)
 {    
     input = input.OrderBy(e => rand.Next()).ToList();
-    double[,] matrix = new double[4, 5];
+    List<List<double>> matrix = new();
     for(int i = 1; i < 5; ++i)
     {
-        matrix[i - 1, 0] = input[i][4] - input[0][4];
-        matrix[i - 1, 1] = input[0][3] - input[i][3];
-        matrix[i - 1, 2] = input[0][1] - input[i][1];
-        matrix[i - 1, 3] = input[i][0] - input[0][0];
-        matrix[i - 1, 4] = input[0][1] * input[0][3] - input[i][1] * input[i][3] + input[i][0] * input[i][4] - input[0][0] * input[0][4];
+        matrix.Add(new());
+        matrix[i - 1].Add(input[i][VY] - input[0][VY]);
+        matrix[i - 1].Add(input[0][VX] - input[i][VX]);
+        matrix[i - 1].Add(input[0][Y] - input[i][Y]);
+        matrix[i - 1].Add(input[i][X] - input[0][X]);
+        matrix[i - 1].Add(input[0][Y] * input[0][VX] - input[i][Y] * input[i][VX] + input[i][X] * input[i][VY] - input[0][X] * input[0][VY]);
     }
     return matrix;
 }
 
-double[,] BuildMatrix2(List<List<long>> input, double x_0, double y_0, double v_x0, double v_y0)
+List<List<double>> BuildMatrix2(List<List<long>> input)
 {
     input = input.OrderBy(e => rand.Next()).ToList();
-    double[,] matrix = new double[4, 5];
+    List<List<double>> matrix = new();
     for (int i = 1; i < 5; ++i)
     {
-        matrix[i - 1, 0] = input[i][5] - input[0][5];
-        matrix[i - 1, 1] = input[0][3] - input[i][3];
-        matrix[i - 1, 2] = input[0][2] - input[i][2];
-        matrix[i - 1, 3] = input[i][0] - input[0][0];
-        matrix[i - 1, 4] = input[0][2] * input[0][3] - input[i][2] * input[i][3] + input[i][0] * input[i][5] - input[0][0] * input[0][5];
+        matrix.Add(new());
+        matrix[i - 1].Add(input[i][VZ] - input[0][VZ]);
+        matrix[i - 1].Add(input[0][VX] - input[i][VX]);
+        matrix[i - 1].Add(input[0][Z] - input[i][Z]);
+        matrix[i - 1].Add(input[i][X] - input[0][X]);
+        matrix[i - 1].Add(input[0][Z] * input[0][VX] - input[i][Z] * input[i][VX] + input[i][X] * input[i][VZ] - input[0][X] * input[0][VZ]);
     }
     return matrix;
 }
@@ -95,7 +104,8 @@ double[,] BuildMatrix2(List<List<long>> input, double x_0, double y_0, double v_
 void part2(string file_name)
 {
     List<List<long>> input = readFileLines(file_name).ExtractSignedLongs().Select(e => e.ToList()).ToList();
-    List<List<long>> answers = new();
+
+    int RESULT = 4;
 
     long t_dec = 0;
     long v_x0_dec = 0;
@@ -107,21 +117,21 @@ void part2(string file_name)
 
     do
     {
-        double[,] matrix = BuildMatrix1(input);
+        List<List<double>> matrix = BuildMatrix1(input);
         if(LinearEquationSolver.Solve(matrix))
         {
-            double x_0 = matrix[0, 4];
-            double y_0 = matrix[1, 4];
-            double v_x0 = matrix[2, 4];
-            double v_y0 = matrix[3, 4];
+            double x_0 = matrix[0][RESULT];
+            double y_0 = matrix[1][RESULT];
+            double v_x0 = matrix[2][RESULT];
+            double v_y0 = matrix[3][RESULT];
 
-            matrix = BuildMatrix2(input, x_0, y_0, v_x0, v_y0);
+            matrix = BuildMatrix2(input);
             if(LinearEquationSolver.Solve(matrix))
             {
-                double z_0 = matrix[1, 4];
-                double v_z0 = matrix[3, 4];
+                double z_0 = matrix[1][RESULT];
+                double v_z0 = matrix[3][RESULT];
 
-                t_dec = (long)Math.Round((x_0 - input[0][0]) / (input[0][3] - v_x0));
+                t_dec = (long)Math.Round((x_0 - input[0][X]) / (input[0][VX] - v_x0));
                 v_x0_dec = (long)Math.Round(v_x0);
                 x_dec = (long)Math.Round(x_0);
                 v_y0_dec = (long)Math.Round(v_y0);
@@ -130,7 +140,7 @@ void part2(string file_name)
                 z_dec = (long)Math.Round(z_0);
             }
         }
-    } while ((x_dec + v_x0_dec * t_dec != input[0][0] + input[0][3] * t_dec) || (y_dec + v_y0_dec * t_dec != input[0][1] + input[0][4] * t_dec) || (z_dec + v_z0_dec * t_dec != input[0][2] + input[0][5] * t_dec));
+    } while ((x_dec + v_x0_dec * t_dec != input[0][X] + input[0][VX] * t_dec) || (y_dec + v_y0_dec * t_dec != input[0][Y] + input[0][VY] * t_dec) || (z_dec + v_z0_dec * t_dec != input[0][Z] + input[0][VZ] * t_dec));
 
     long sum = x_dec + y_dec + z_dec;
 
@@ -146,80 +156,16 @@ part2("input.txt");
 
 public static class LinearEquationSolver
 {
-    public static bool Solve(double[,] M)
+    public static bool Solve(List<List<double>> matrix)
     {
-        int rowCount = M.GetLength(0);
-
-        for (int col = 0; col + 1 < rowCount; col++)
-        {
-            if (M[col, col] == 0)
-            {
-                int swapRow = col + 1;
-                while(swapRow < rowCount && M[swapRow, col] == 0)
-                {
-                    ++swapRow;
-                }
-
-                if(M[swapRow, col] == 0)
-                {
-                    return false;
-                }
-
-                double tmp = 0;
-                for (int i = 0; i < rowCount + 1; i++)
-                {
-                    tmp = M[swapRow, i];
-                    M[swapRow, i] = M[col, i];
-                    M[col, i] = tmp;
-                }
-            }
-        }
-
-        for (int sourceRow = 0; sourceRow + 1 < rowCount; sourceRow++)
-        {
-            for (int destRow = sourceRow + 1; destRow < rowCount; destRow++)
-            {
-                double df = M[sourceRow, sourceRow];
-                double sf = M[destRow, sourceRow];
-                for (int i = 0; i < rowCount + 1; i++)
-                {
-                    M[destRow, i] = M[destRow, i] * df - M[sourceRow, i] * sf;
-                }
-            }
-        }
-
-        for (int row = rowCount - 1; row >= 0; row--)
-        {
-            double f = M[row, row];
-            if (f == 0)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < rowCount + 1; i++)
-            {
-                M[row, i] /= f;
-            }
-
-            for (int destRow = 0; destRow < row; destRow++)
-            {
-                M[destRow, rowCount] -= M[destRow, row] * M[row, rowCount];
-                M[destRow, row] = 0;
-            }
-        }
-
-        return true;
-    }
-
-    public static bool Solve(double[][] matrix)
-    {
-        int rowCount = matrix.GetLength(0);
+        int rowCount = matrix.Count;
 
         for (int col = 0; col + 1 < rowCount; col++)
         {
             if (matrix[col][col] == 0)
             {
                 int swapRow = col + 1;
+
                 while (swapRow < rowCount && matrix[swapRow][col] == 0)
                 {
                     ++swapRow;
@@ -230,47 +176,30 @@ public static class LinearEquationSolver
                     return false;
                 }
 
-                double tmp = 0;
-                for (int i = 0; i < rowCount + 1; i++)
-                {
-                    tmp = matrix[swapRow][i];
-                    matrix[swapRow][i] = matrix[col][i];
-                    matrix[col][i] = tmp;
-                }
+                (matrix[swapRow], matrix[col]) = (matrix[col], matrix[swapRow]);
             }
         }
 
         for (int sourceRow = 0; sourceRow + 1 < rowCount; sourceRow++)
         {
+            double df = matrix[sourceRow][sourceRow];
             for (int destRow = sourceRow + 1; destRow < rowCount; destRow++)
             {
-                double df = matrix[sourceRow][sourceRow];
                 double sf = matrix[destRow][sourceRow];
-                for (int i = 0; i < rowCount + 1; i++)
-                {
-                    matrix[destRow][i] = matrix[destRow][i] * df - matrix[sourceRow][i] * sf;
-                }
+                matrix[destRow] = matrix[destRow].Zip(matrix[sourceRow]).Select(e => e.First * df - e.Second * sf).ToList();
             }
         }
 
         for (int row = rowCount - 1; row >= 0; row--)
         {
-            double f = matrix[row][row];
-            if (f == 0)
+            if (matrix[row][row] == 0)
             {
                 return false;
             }
 
-            for (int i = 0; i < rowCount + 1; i++)
-            {
-                matrix[row][i] /= f;
-            }
-
-            for (int destRow = 0; destRow < row; destRow++)
-            {
-                matrix[destRow][rowCount] -= matrix[destRow][row] * matrix[row][rowCount];
-                matrix[destRow][row] = 0;
-            }
+            matrix[row] = matrix[row].Select(e => e /= matrix[row][row]).ToList();
+            Enumerable.Range(0, row).ToList().ForEach(i => matrix[i][rowCount] -= matrix[i][row] * matrix[row][rowCount]);
+            Enumerable.Range(0, row).ToList().ForEach(i => matrix[i][row] = 0);
         }
 
         return true;
