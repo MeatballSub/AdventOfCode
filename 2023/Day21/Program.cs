eba52458d8391fffb0db56eb139d166cbe2feb5a
Kevin Day <kevinday.home@gmail.com>
2024-01-13 15:59:01 -0600

    Day 21 part 2

--- 2023/Day21/Program.cs
+++ 2023/Day21/Program.cs
@@ -20,17 +20,11 @@ Point FindStart(char[][] grid)
     throw new IndexOutOfRangeException();
 }
 
-void FitAndExtrapolate(List<Point> points, long X)
-{
-
-}
-
-long solve(char[][] grid, Point start, long steps)
+long solve1(char[][] grid, Point start, long steps)
 {
     long count = 0;
-    long show_me = 0;
     HashSet<Point> visited = new();
-    //HashSet<Point> reachable = new();
+    HashSet<Point> reachable = new();
     Queue<(Point, long)> frontier = new();
     List<Point> edge_counts = new();
     frontier.Enqueue((start, 0));
@@ -40,19 +34,9 @@ long solve(char[][] grid, Point start, long steps)
         (Point p, long taken) = frontier.Dequeue();
         if (grid.modAt(p) == '#' || visited.Contains(p) || taken > steps) continue;
         visited.Add(p);
-        if(p.Y % grid.Length == -1 && p.Y < show_me)
-        {
-            show_me = p.Y;
-            edge_counts.Add(new Point(taken - 1, count));
-            if (edge_counts.Count > 1)
-            {
-                FitAndExtrapolate(edge_counts, steps);
-            }
-            Console.WriteLine($"taken: {taken - 1}, count: {count}");
-        }
         if(taken % 2 == steps % 2)
         {
-            //reachable.Add(p);
+            reachable.Add(p);
             ++count;
         }
         p.orthogonalNeighbors().ToList().ForEach(neighbor => frontier.Enqueue((neighbor, taken + 1)));
@@ -77,41 +61,131 @@ long solve(char[][] grid, Point start, long steps)
     return count;
 }
 
+long countGrid(char [][] grid, HashSet<Point> reached, Point min)
+{
+    Point max = new Point(min.X + grid.Length, min.Y + grid.Length);
+    HashSet<Point> subset = reached.Where(p => min.X <= p.X && p.X < max.X && min.Y <= p.Y && p.Y < max.Y).ToHashSet();
+
+    //char[][] reachedGrid = new char[grid.Length][];
+    //for(int y = 0; y < grid.Length; ++y)
+    //{
+    //    reachedGrid[y] = new char[grid.Length];
+    //    for(int x = 0; x < grid.Length; ++x)
+    //    {
+    //        reachedGrid[y][x] = grid[y][x];
+    //    }
+    //}
+
+    //foreach(Point p in subset)
+    //{
+    //    reachedGrid.modAt(p) = 'O';
+    //}
+
+    //Console.WriteLine();
+    //for (int y = 0; y < reachedGrid.Length; ++y)
+    //{
+    //    for (int x = 0; x < reachedGrid.Length; ++x)
+    //    {
+    //        Console.Write(reachedGrid[y][x]);
+    //    }
+    //    Console.WriteLine();
+    //}
+    //Console.WriteLine();
+
+    return subset.Count;
+}
+
+long solve2(char[][] grid, Point start, long steps)
+{
+    // taking advantage of input:
+    // * start at center
+    // * steps is an exact multiple of grid.Length + steps to center
+    // * that exact multiple is even
+    long evens = ((steps - start.X) / grid.Length) - 1;
+    long odds = evens + 1;
+    long min_steps = grid.Length * 4 + start.X;
+
+    HashSet<Point> visited = new();
+    HashSet<Point> reachable = new();
+    Queue<(Point, long)> frontier = new();
+    List<Point> edge_counts = new();
+    frontier.Enqueue((start, 0));
+        
+    while (frontier.Count > 0)
+    {
+        (Point p, long taken) = frontier.Dequeue();
+        if (grid.modAt(p) == '#' || visited.Contains(p) || taken > min_steps) continue;
+        visited.Add(p);
+        if (taken % 2 == min_steps % 2)
+        {
+            reachable.Add(p);
+        }
+        p.orthogonalNeighbors().ToList().ForEach(neighbor => frontier.Enqueue((neighbor, taken + 1)));
+    }
+
+    Dictionary<string, Point> gridTL = new()
+    {
+        { "even full", new Point(0, 0) },
+        { "odd full", new Point(0, grid.Length) },
+        { "NE outer", new Point(grid.Length * 2, -grid.Length * 3) },
+        { "SE outer", new Point(grid.Length * 2, grid.Length * 3) },
+        { "SW outer", new Point(-grid.Length * 2, grid.Length * 3) },
+        { "NW outer", new Point(-grid.Length * 2, -grid.Length * 3) },
+        { "NE inner", new Point(grid.Length * 2, -grid.Length * 2) },
+        { "SE inner", new Point(grid.Length * 2, grid.Length * 2) },
+        { "SW inner", new Point(-grid.Length * 2, grid.Length * 2) },
+        { "NW inner", new Point(-grid.Length * 2, -grid.Length * 2) },
+        { "N tip", new Point(0, -grid.Length * 4) },
+        { "E tip", new Point(grid.Length * 4, 0) },
+        { "S tip", new Point(0, grid.Length * 4) },
+        { "W tip", new Point(-grid.Length * 4, 0) },
+    };
+
+    long full_count = evens * evens * countGrid(grid, reachable, gridTL["even full"]) +
+                       odds *  odds * countGrid(grid, reachable, gridTL["odd full"]) +
+                               odds * countGrid(grid, reachable, gridTL["NE outer"]) +
+                               odds * countGrid(grid, reachable, gridTL["SE outer"]) +
+                               odds * countGrid(grid, reachable, gridTL["SW outer"]) +
+                               odds * countGrid(grid, reachable, gridTL["NW outer"]) +
+                              evens * countGrid(grid, reachable, gridTL["NE inner"]) +
+                              evens * countGrid(grid, reachable, gridTL["SE inner"]) +
+                              evens * countGrid(grid, reachable, gridTL["SW inner"]) +
+                              evens * countGrid(grid, reachable, gridTL["NW inner"]) +
+                                      countGrid(grid, reachable, gridTL["N tip"]) +
+                                      countGrid(grid, reachable, gridTL["E tip"]) +
+                                      countGrid(grid, reachable, gridTL["S tip"]) +
+                                      countGrid(grid, reachable, gridTL["W tip"]);
+
+    //Console.WriteLine($"evens = {evens}, odds = {odds}");
+    return full_count;
+}
+
 void part1(string file_name, long steps, long expected)
 {
     Console.OutputEncoding = Encoding.UTF8;
     char[][] grid = readFileLines(file_name).Select(l => l.ToCharArray()).ToArray();
     Point start = FindStart(grid);
-    long count = solve(grid, start, steps);
+    long count = solve1(grid, start, steps);
     Console.WriteLine($"Part 1 - {file_name}: {count} {(count == expected ? "\u2705" : "\u274c")}");
 }
 
-// 623131417303078 is too high
 void part2(string file_name, long steps, long expected)
 {
     Console.OutputEncoding = Encoding.UTF8;
     char[][] grid = readFileLines(file_name).Select(l => l.ToCharArray()).ToArray();
     Point start = FindStart(grid);
-    long count = solve(grid, start, steps);
+    long count = solve2(grid, start, steps);
     Console.WriteLine($"Part 2 - {file_name}: {count} {(count == expected ? "\u2705" : "\u274c")}");
 }
 
-//part1("sample.txt", 1, 2);
-//part1("sample.txt", 2, 4);
-//part1("sample.txt", 3, 6);
-//part1("sample.txt", 4, 9);
-//part1("sample.txt", 5, 13);
-//part1("sample.txt", 6, 16);
-//part1("input.txt", 64, 3699);
-//part2("sample.txt", 6, 16);
-//part2("sample.txt", 10, 50);
-//part2("sample.txt", 50, 1594);
-//part2("sample.txt", 100, 6536);
-//part2("sample.txt", 500, 167004);
-//part2("sample.txt", 1000, 668697);
-//part2("sample.txt", 5000, 16733044);
-//part2("sample.txt", 15, 115);
-part2("input.txt", 26501365, 0);
+part1("sample.txt", 1, 2);
+part1("sample.txt", 2, 4);
+part1("sample.txt", 3, 6);
+part1("sample.txt", 4, 9);
+part1("sample.txt", 5, 13);
+part1("sample.txt", 6, 16);
+part1("input.txt", 64, 3699);
+part2("input.txt", 26501365, 613391294577878);
 
 static class Extensions
 {
