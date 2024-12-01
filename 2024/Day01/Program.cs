21f617b5aea75d61a55438758f4f7a719be4e7c7
Kevin Day <kevinday.home@gmail.com>
2024-11-30 23:37:55 -0600

    2024 - Day 1

--- 2024/Day01/Program.cs
+++ 2024/Day01/Program.cs
@@ -1,6 +1,42 @@
 using Library;
-using static Library.Geometry;
-using static Library.Optimize;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+(IEnumerable<long>, IEnumerable<long>) parse(string file_name)
+{
+    var lists_values = readFileLines(file_name).Select(l => l.ExtractLongs());
+    var left = lists_values.Select(v => v.First());
+    var right = lists_values.Select(v => v.Last());
+    return (left, right);
+}
+
+long solve_part1(string file_name)
+{
+    var (left, right) = parse(file_name);
+
+    return left.Order().Zip(right.Order()).Select(pair => Math.Abs(pair.First - pair.Second)).Sum();
+}
+
+long solve_part2(string file_name)
+{
+    var (left, right) = parse(file_name);
+    var counts = right.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+    return left.Where(v => counts.ContainsKey(v)).Sum(v => v * counts[v]);
+}
+
+void part1(string file_name)
+{
+    long solution = solve_part1(file_name);
+    Console.WriteLine($"Part 1 - {file_name}: {solution}");
+}
+
+void part2(string file_name)
+{
+    long solution = solve_part2(file_name);
+    Console.WriteLine($"Part 2 - {file_name}: {solution}");
+}
+
+part1("sample.txt");
+part1("input.txt");
+
+part2("sample.txt");
+part2("input.txt");
