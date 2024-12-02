e2ae4485627f943309fb70db7d51491ec398c7f1
Kevin Day <kevinday.home@gmail.com>
2024-12-02 02:01:13 -0600

    Day 2

--- 2024/Day01/Program.cs
+++ 2024/Day01/Program.cs
@@ -9,29 +9,20 @@ using static Library.Parsing;
     return (left, right);
 }
 
-long solve_part1(string file_name)
-{
-    var (left, right) = parse(file_name);
-
-    return left.Order().Zip(right.Order()).Select(pair => Math.Abs(pair.First - pair.Second)).Sum();
-}
-
-long solve_part2(string file_name)
-{
-    var (left, right) = parse(file_name);
-    var counts = right.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
-    return left.Where(v => counts.ContainsKey(v)).Sum(v => v * counts[v]);
-}
-
 void part1(string file_name)
 {
-    long solution = solve_part1(file_name);
+    var (left, right) = parse(file_name);
+    long solution = left.Order().Zip(right.Order()).Select(pair => Math.Abs(pair.First - pair.Second)).Sum();
+
     Console.WriteLine($"Part 1 - {file_name}: {solution}");
 }
 
 void part2(string file_name)
 {
-    long solution = solve_part2(file_name);
+    var (left, right) = parse(file_name);
+    var counts = right.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+    long solution = left.Where(v => counts.ContainsKey(v)).Sum(v => v * counts[v]);
+
     Console.WriteLine($"Part 2 - {file_name}: {solution}");
 }
 
--- 2024/Day02/Program.cs
+++ 2024/Day02/Program.cs
@@ -1,6 +1,26 @@
 using Library;
-using static Library.Geometry;
-using static Library.Optimize;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+bool IsOrderedStrict(IEnumerable<long> enumerable) => enumerable.IsOrderedStrictAsc() || enumerable.IsOrderedStrictDesc();
+long MaxDiff(IEnumerable<long> enumerable) => enumerable.Zip(enumerable.Skip(1), (a, b) => Math.Abs(a - b)).Max();
+bool IsSafe(IEnumerable<long> enumerable) => IsOrderedStrict(enumerable) && MaxDiff(enumerable) < 4;
+IEnumerable<IEnumerable<long>>  excludeOne(IEnumerable<long> report) => report.Select((_, i) => report.Where((v, index) => index != i));
+
+void part1(string file_name)
+{
+    var safe_reports = readFileLines(file_name).Select(l => l.ExtractLongs()).Where(IsSafe);
+    Console.WriteLine($"part 1 - {file_name}: {safe_reports.Count()}");
+}
+
+void part2(string file_name)
+{
+    var safe_reports = readFileLines(file_name).Select(l => l.ExtractLongs()).Where(r => IsSafe(r) || excludeOne(r).Any(IsSafe));
+    Console.WriteLine($"part 2 - {file_name}: {safe_reports.Count()}");
+}
+
+
+part1("sample.txt");
+part1("input.txt");
+
+part2("sample.txt");
+part2("input.txt");
