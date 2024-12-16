71cabbf49567c595ff2f4efceb29209cfe28a5dd
Kevin Day <kevinday.home@gmail.com>
2024-12-16 16:34:17 -0600

    Update template

--- 2024/Day17/Program.cs
+++ 2024/Day17/Program.cs
@@ -1,23 +1,41 @@
+using System.Numerics;
 using static Library.Parsing;
 
-void part1(string file_name)
+long part1(string file_name)
 {
-    var solution = 0;
+    var solution = 0L;
     var input = readFileLines(file_name);
 
-    Console.WriteLine($"part 1 - {file_name}: {solution}");
+    return solution;
 }
 
-void part2(string file_name)
+long part2(string file_name)
 {
-    var solution = 0;
+    var solution = 0L;
     var input = readFileLines(file_name);
 
-    Console.WriteLine($"part 2 - {file_name}: {solution}");
+    return solution;
 }
 
-part1("sample.txt");
-part1("input.txt");
+void test<T>(Func<string, T> test_func, string func_name, string file_name, T expected) where T : IEqualityOperators<T, T, bool>
+{
+    var old_color = Console.ForegroundColor;
+    var actual = test_func(file_name);
+    if (actual == expected)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{func_name} - {file_name}: {actual}");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{func_name} - {file_name}[ actual: {actual} expected: {expected} ]");
+    }
+    Console.ForegroundColor = old_color;
+}
 
-part2("sample.txt");
-part2("input.txt");
+test(part1, "part1", "sample.txt", 0);
+test(part1, "part1", "input.txt", 0);
+
+test(part2, "part2", "sample.txt", 0);
+test(part2, "part2", "input.txt", 0);
--- 2024/Day18/Program.cs
+++ 2024/Day18/Program.cs
@@ -1,6 +1,41 @@
-using Library;
-using static Library.Geometry;
-using static Library.Optimize;
+using System.Numerics;
 using static Library.Parsing;
 
-Console.WriteLine("Hello World!");
+long part1(string file_name)
+{
+    var solution = 0L;
+    var input = readFileLines(file_name);
+
+    return solution;
+}
+
+long part2(string file_name)
+{
+    var solution = 0L;
+    var input = readFileLines(file_name);
+
+    return solution;
+}
+
+void test<T>(Func<string, T> test_func, string func_name, string file_name, T expected) where T : IEqualityOperators<T, T, bool>
+{
+    var old_color = Console.ForegroundColor;
+    var actual = test_func(file_name);
+    if (actual == expected)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{func_name} - {file_name}: {actual}");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{func_name} - {file_name}[ actual: {actual} expected: {expected} ]");
+    }
+    Console.ForegroundColor = old_color;
+}
+
+test(part1, "part1", "sample.txt", 0);
+test(part1, "part1", "input.txt", 0);
+
+test(part2, "part2", "sample.txt", 0);
+test(part2, "part2", "input.txt", 0);
