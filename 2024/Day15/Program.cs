dfa6482835e351e147a3d847633e19dd81da2634
Kevin Day <kevinday.home@gmail.com>
2024-12-16 16:19:08 -0600

    Cleanup Day 15

--- 2024/Day15/Program.cs
+++ 2024/Day15/Program.cs
@@ -11,13 +11,14 @@ Dictionary<char, Func<Point, Point>> move_functions = new()
     { '<', p => p.Left() },
 };
 
-Dictionary<char, Func<Point, Point>> swap_functions = new()
+(char[][] map, string moves) parse(string file_name)
 {
-    { '^', p => p.Down() },
-    { '>', p => p.Left() },
-    { 'v', p => p.Up() },
-    { '<', p => p.Right() },
-};
+    var parts = SplitBlankLine(file_name);
+    var map = parts[0].SplitLines().Select(l => l.ToCharArray()).ToArray();
+    var moves = parts[1].Replace(Environment.NewLine, String.Empty);
+
+    return (map, moves);
+}
 
 void findRobot(ref char[][] map, out Point? robot_position)
 {
@@ -37,48 +38,29 @@ void findRobot(ref char[][] map, out Point? robot_position)
     }
 }
 
-(char[][] map, string moves, Point robot_position) parse1(string file_name)
+char[][] expandMap(ref char[][] map)
 {
-    var parts = SplitBlankLine(file_name);
-    var map = parts[0].SplitLines().Select(l => l.ToCharArray()).ToArray();
-    var moves = parts[1].Replace(Environment.NewLine, String.Empty);
-    findRobot(ref map, out var robot_position);
-
-    if (robot_position == null) throw new Exception("Couldn't find robot");
-
-    return (map, moves, robot_position);
-}
-
-(char[][] map, string moves, Point robot_position) parse2(string file_name)
-{
-    var parts = SplitBlankLine(file_name);
-    var original_map = parts[0].SplitLines().Select(l => l.ToCharArray()).ToArray();
-    var moves = parts[1].Replace("\n", String.Empty).Replace("\r", String.Empty);
-    Point robot_position = new Point(-1, -1);
-
-    // Double map
     List<string> map_buffer = new();
-    for(int y = 0; y < original_map.Length; ++y)
+    for (int y = 0; y < map.Length; ++y)
     {
         StringBuilder sb = new();
-        for (int x = 0; x < original_map[y].Length; ++x)
+        for (int x = 0; x < map[y].Length; ++x)
         {
-            if(original_map[y][x] == '#')
+            if (map[y][x] == '#')
             {
                 sb.Append("##");
             }
-            else if (original_map[y][x] == 'O')
+            else if (map[y][x] == 'O')
             {
                 sb.Append("[]");
             }
-            else if(original_map[y][x] == '.')
+            else if (map[y][x] == '.')
             {
                 sb.Append("..");
             }
-            else if(original_map[y][x] == '@')
+            else if (map[y][x] == '@')
             {
-                sb.Append("..");
-                robot_position = new Point(x * 2, y);
+                sb.Append("@.");
             }
             else
             {
@@ -88,198 +70,102 @@ void findRobot(ref char[][] map, out Point? robot_position)
         map_buffer.Add(sb.ToString());
     }
 
-    var double_map = map_buffer.Select(l => l.ToCharArray()).ToArray();
-
-    return (double_map, moves, robot_position);
+    return map_buffer.Select(l => l.ToCharArray()).ToArray();
 }
 
-void sim1(ref char[][] map, string moves, Point robot_start_position)
+List<Point> getNeighbors(ref char[][] map, char move, Point curr)
+{
+    List<Point> neighbors = new();
+    var curr_value = map.at(curr);
+    if (curr_value == '#') return neighbors;
+
+    var neighbor = move_functions[move](curr);
+    var neighbor_value = map.at(neighbor);
+
+    if (move == 'v' || move == '^')
+    {
+        if (neighbor_value == '[')
+        {
+            neighbors.Add(neighbor.Right());
+        }
+        else if (neighbor_value == ']')
+        {
+            neighbors.Add(neighbor.Left());
+        }
+    }
+
+    if (neighbor_value != '.')
+    {
+        neighbors.Add(neighbor);
+    }
+
+    return neighbors;
+}
+
+(bool valid, HashSet<Point> obj) buildObject(ref char[][] map, char move, Point robot_position)
+{
+    bool valid = true;
+    HashSet<Point> obj = new();
+    Queue<Point> queue = new();
+    queue.Enqueue(robot_position);
+    while (queue.TryDequeue(out var curr))
+    {
+        obj.Add(curr);
+        if (map.at(curr) == '#')
+        {
+            valid = false;
+            break;
+        }
+
+        foreach (var neighbor in getNeighbors(ref map, move, curr))
+        {
+            queue.Enqueue(neighbor);
+        }
+    }
+    return (valid, obj);
+}
+
+void moveObject(ref char[][]map, char move, HashSet<Point> obj)
+{
+    List<(Point, char)> move_values = new();
+    foreach (Point p in obj)
+    {
+        move_values.Add((p, map.at(p)));
+    }
+    foreach (var (p, v) in move_values)
+    {
+        map[p.Y][p.X] = '.';
+    }
+    foreach (var (p, v) in move_values)
+    {
+        var np = move_functions[move](p);
+        map[np.Y][np.X] = v;
+    }
+}
+
+void sim(ref char[][] map, string moves, Point robot_start_position)
 {
     Point robot_position = robot_start_position;
-    foreach(char move in moves)
-    {
-        Point end_point = move_functions[move](robot_position);
-        while (map.at(end_point) != '#' && map.at(end_point) != '.')
-        {
-            end_point = move_functions[move](end_point);
-        }
-        if (map.at(end_point) == '.')
-        {
-            while (!end_point.Equals(robot_position))
-            {
-                Point swap_point = swap_functions[move](end_point);
-                char temp = map.at(swap_point);
-                map[swap_point.Y][swap_point.X] = map.at(end_point);
-                map[end_point.Y][end_point.X] = temp;
-                end_point = swap_point;
-            }
-            robot_position = move_functions[move](robot_position);
-        }
-    }
-}
-
-bool keepTrying(ref char[][]map, HashSet<Point> check_points)
-{
-    foreach(Point p in check_points)
-    {
-        if (map.at(p) == '#') return false;
-    }
-
-    foreach(Point p in check_points)
-    {
-        if (map.at(p) != '.')
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-(bool, HashSet<Point>) isPossible(ref char[][]map, char move, Point robot_position)
-{
-    Point attempt_move_point = move_functions[move](robot_position);
-    List<Point> end_points = new();
-    HashSet<Point> new_end_points = new() { attempt_move_point };
-    char map_value = map.at(attempt_move_point);
-    if(move == 'v' || move == '^')
-    {
-        if (map_value == ']')
-        {
-            new_end_points.Add(attempt_move_point.Left());
-        }
-        else if (map_value == '[')
-        {
-            new_end_points.Add(attempt_move_point.Right());
-        }
-    }
-
-    while (keepTrying(ref map, new_end_points))
-    {
-        end_points.AddRange(new_end_points);
-        new_end_points = new_end_points.Select(p => move_functions[move](p)).ToHashSet();
-        foreach(Point p in new_end_points.ToList())
-        {
-            map_value = map.at(p);
-            if(map_value == '.')
-            {
-                new_end_points.Remove(p);
-            }
-            else if (move == 'v' || move == '^')
-            {
-                if (map_value == ']')
-                {
-                    new_end_points.Add(p.Left());
-                }
-                else if (map_value == '[')
-                {
-                    new_end_points.Add(p.Right());
-                }
-            }
-        }
-    }
-
-    foreach(Point p in new_end_points)
-    {
-        if (map.at(p) == '#' || map.at(p) != '.') return (false, new HashSet<Point>());
-    }
-    return (true, end_points.ToHashSet());
-}
-
-void sim2(ref char[][] map, string moves, Point robot_start_position)
-{
-    Point robot_position = robot_start_position;
-
-    //Console.WriteLine();
-    //for (int y = 0; y < map.Length; y++)
-    //{
-    //    for (int x = 0; x < map[y].Length; x++)
-    //    {
-    //        if (y == robot_position.Y && x == robot_position.X)
-    //        {
-    //            Console.Write('@');
-    //        }
-    //        else
-    //        {
-    //            Console.Write(map[y][x]);
-    //        }
-    //    }
-    //    Console.WriteLine();
-    //}
-
     foreach (char move in moves)
     {
-        //Console.WriteLine();
-        //Console.WriteLine(move);
-        var (is_possible, move_points) = isPossible(ref map, move, robot_position);
-        if (is_possible)
+        var (valid_move, obj_to_move) = buildObject(ref map, move, robot_position);
+        if(valid_move)
         {
-            List<(Point, char)> move_values = new();
-            foreach(Point p in move_points)
-            {
-                move_values.Add((p, map.at(p)));
-            }
-            foreach(var (p, v) in move_values)
-            {
-                map[p.Y][p.X] = '.';
-            }
-            foreach (var (p, v) in move_values)
-            {
-                var np = move_functions[move](p);
-                map[np.Y][np.X] = v;
-            }
+            moveObject(ref map, move, obj_to_move);
             robot_position = move_functions[move](robot_position);
         }
-
-        //Console.WriteLine();
-        //for (int y = 0; y < map.Length; y++)
-        //{
-        //    for (int x = 0; x < map[y].Length; x++)
-        //    {
-        //        if (y == robot_position.Y && x == robot_position.X)
-        //        {
-        //            Console.Write('@');
-        //        }
-        //        else
-        //        {
-        //            Console.Write(map[y][x]);
-        //        }
-        //    }
-        //    Console.WriteLine();
-        //}
-        //Console.WriteLine();
-        //Console.WriteLine();
     }
 }
 
-long part1(string file_name)
+long score(ref char[][] map, char score_symobl)
 {
     var solution = 0L;
-    var (map, moves, robot_position) = parse1(file_name);
-    sim1(ref map, moves, robot_position);
-    for(int y = 0; y < map.Length; y++)
-    {
-        for(int x = 0; x < map[y].Length; x++)
-        {
-            if (map[y][x] == 'O')
-            {
-                solution += 100 * y + x;
-            }
-        }
-    }
 
-    return solution;
-}
-
-long part2(string file_name)
-{
-    var solution = 0L;
-    var (map, moves, robot_position) = parse2(file_name);
-    sim2(ref map, moves, robot_position);
     for (int y = 0; y < map.Length; y++)
     {
         for (int x = 0; x < map[y].Length; x++)
         {
-            if (map[y][x] == '[')
+            if (map[y][x] == score_symobl)
             {
                 solution += 100 * y + x;
             }
@@ -287,6 +173,29 @@ long part2(string file_name)
     }
 
     return solution;
+}
+
+long part1(string file_name)
+{
+    var (map, moves) = parse(file_name);
+
+    findRobot(ref map, out var robot_position);
+    if (robot_position == null) throw new Exception("Couldn't find robot");
+
+    sim(ref map, moves, robot_position);
+    return score(ref map, 'O');
+}
+
+long part2(string file_name)
+{
+    var (original_map, moves) = parse(file_name);
+    var map = expandMap(ref original_map);
+
+    findRobot(ref map, out var robot_position);
+    if (robot_position == null) throw new Exception("Couldn't find robot");
+
+    sim(ref map, moves, robot_position);
+    return score(ref map, '[');
 }
 
 void test<T>(Func<string, T> test_func, string func_name, string file_name, T expected) where T: IEqualityOperators<T,T,bool>
