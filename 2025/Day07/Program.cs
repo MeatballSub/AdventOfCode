c180c368016d9b448cefb586842f949ac520efd8
Kevin Day <kevinday.home@gmail.com>
2025-12-08 19:47:18 -0600

    2025 - Day 7

--- 2025/Day07/Program.cs
+++ 2025/Day07/Program.cs
@@ -1,24 +1,66 @@
+using Library;
 using static Library.Parsing;
 using static Library.Testing;
 
-long part1(string file_name)
+void add_beam(Dictionary<Geometry.Point, long> beams, Geometry.Point key, long count)
+{
+    if (beams.ContainsKey(key))
+    {
+        beams[key] += count;
+    }
+    else
+    {
+        beams.Add(key, count);
+    }
+}
+
+long solve(char[][] input, Func<long, Dictionary<Geometry.Point, long>, long> update_solution)
 {
     var solution = 0L;
-    var input = readFileLines(file_name);
+    var (_, start_loc) = input.find('S');
+    Dictionary<Geometry.Point, long> beams = new() { { start_loc, 1 } };
+    while (beams.Count > 0)
+    {
+        beams = beams.Where(b => b.Key.Y < input.Length - 1).ToDictionary();
+        var classified_beams = beams.ToLookup(b => input.at(b.Key.Down()) == '^');
+        var split_beams = classified_beams[true];
+        var unsplit_beams = classified_beams[false];
+
+        Dictionary<Geometry.Point, long> new_beams = new();
+        foreach (var (beam, count) in split_beams)
+        {
+            add_beam(new_beams, beam.Left().Down(), count);
+            add_beam(new_beams, beam.Right().Down(), count);
+        }
+
+        foreach (var (beam, count) in unsplit_beams)
+        {
+            add_beam(new_beams, beam.Down(), count);
+        }
+
+        solution = update_solution(solution, beams);
+        beams = new_beams;
+    }
 
     return solution;
+}
+
+long part1(string file_name)
+{
+    var input = readFileAsGrid(file_name);
+    var update_solution = (long value, Dictionary<Geometry.Point, long> beams) => value + beams.Where(b => input.at(b.Key.Down()) == '^').ToHashSet().Count();
+    return solve(input, update_solution);
 }
 
 long part2(string file_name)
 {
-    var solution = 0L;
-    var input = readFileLines(file_name);
-
-    return solution;
+    var input = readFileAsGrid(file_name);
+    var update_solution = (long value, Dictionary<Geometry.Point, long> beams) => Math.Max(value, beams.Values.Sum());
+    return solve(input, update_solution);
 }
 
-test(part1, "part1", "sample.txt", 0);
-test(part1, "part1", "input.txt", 0);
+test(part1, "part1", "sample.txt", 21);
+test(part1, "part1", "input.txt", 1555);
 
-test(part2, "part2", "sample.txt", 0);
-test(part2, "part2", "input.txt", 0);
+test(part2, "part2", "sample.txt", 40);
+test(part2, "part2", "input.txt", 12895232295789);
